0c6f16733c23d8ad620ed064bbb773be44ddfccd
ivan.datsiv <ivan.datsiv@mail.ru>
2017-02-21 18:53:03 +0200

    add directives

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,5 +1,6 @@
+import {Ingredient} from '../ingredient';
 export class Recipe {
-  constructor(public name, public description, public imagePath) {
+  constructor(public name, public description, public imagePath, public ingridient: Ingredient []) {
 
   }
 }
